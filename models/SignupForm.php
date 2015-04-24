8df59bfe131f6ee4d57b3e5247734c9a8577fdf2
alvin_vna <alvin_vna@yahoo.com>
2015-04-25 01:18:18 +0700

    ok

---
+++ models/SignupForm.php
@@ -0,0 +1,70 @@
+<?php
+namespace app\models;
+
+use app\models\User;
+use yii\base\Model;
+use Yii;
+
+/**
+ * Signup form
+ */
+class SignupForm extends Model
+{
+    public $first_name;
+    public $last_name;
+    public $username;
+    public $email;
+    public $password;
+
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+
+            ['first_name', 'required','message'=>'Have to fill this field.'],
+            ['first_name', 'string', 'min' => 2, 'max' => 255],
+
+            ['last_name', 'required','message'=>'Have to fill this field.'],
+            ['last_name', 'string', 'min' => 2, 'max' => 255],
+
+            ['username', 'filter', 'filter' => 'trim'],
+            ['username', 'required'],
+            ['username', 'unique', 'targetClass' => '\app\models\User', 'message' => 'This username has already been taken.'],
+            ['username', 'string', 'min' => 2, 'max' => 255],
+
+            ['email', 'filter', 'filter' => 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'unique', 'targetClass' => '\app\models\User', 'message' => 'This email address has already been taken.'],
+
+            ['password', 'required'],
+            ['password', 'string', 'min' => 6],
+        ];
+    }
+
+    /**
+     * Signs user up.
+     *
+     * @return User|null the saved model or null if saving fails
+     */
+    public function signup()
+    {
+        if ($this->validate()) {
+            $user = new User();
+            $user->first_name = $this->first_name;
+            $user->last_name = $this->last_name;
+            $user->username = $this->username;
+            $user->email = $this->email;
+            $user->setPassword($this->password);
+            $user->generateAuthKey();
+            if ($user->save()) {
+                return $user;
+            }
+        }
+
+        return null;
+    }
+}
